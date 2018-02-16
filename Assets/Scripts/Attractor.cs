ea1a6ed4e7474a5559ce8e602c89986dc37cbad6
Cameron Dickie <cameronbdickie@gmail.com>
2018-02-15 19:46:13 -0500

    Initial Commit
    It works! Finally

---
+++ Assets/Scripts/Attractor.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attractor : MonoBehaviour {
+    /*
+     * Cameron Dickie
+     * December 21st 2017
+     * Creates a script to attract two objects with a rigidbody together
+     */
+    
+
+    const float G = 6674f;
+
+    public static List<Attractor> Attractors;
+
+    public Rigidbody2D rb;
+    public float limit;
+    private void FixedUpdate()
+    {
+        
+        foreach (Attractor attractor in Attractors)
+        {
+            if (attractor != this)
+            {
+                Attract(attractor); // attracts all objects in the static list
+            }
+        }
+    }
+
+    void OnEnable()
+    {
+        if (Attractors == null)
+        {
+            Attractors = new List<Attractor>(); //init array if empty
+        }
+        Attractors.Add(this); // add this attractor to the static list
+    }
+    void Attract(Attractor objToAttract)
+    {
+        if(objToAttract == null)
+        {
+            return;
+        }
+        Rigidbody2D rbToAttract = objToAttract.rb;
+
+        Vector3 direction = rb.position - rbToAttract.position; // find the direction to attract 
+        float distance = direction.magnitude;
+
+        
+        float forceMagnitude = G* (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
+        
+        if(distance > limit)
+        {
+            forceMagnitude = 0;
+        }
+        Vector3 force = direction.normalized * forceMagnitude; // creating a vector of the force and direction
+ 
+        rbToAttract.AddForce(force);
+        
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public Rigidbody2D rb;
+
+    public float speed;
+    public float jumpVelocity;
+    private float fallMultiplier;
+    private float lowJumpMultiplier;
+
+    private bool isGrounded;
+    public float limit;
+    private int jumps;
+
+    private void Awake()
+    {
+        jumps = 0;
+        rb = GetComponent<Rigidbody2D>();
+        fallMultiplier = 2.5f;
+        lowJumpMultiplier = 2f;
+    }
+    private void FixedUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && jumps > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.AddForce(jumpVelocity * Vector2.up);
+            jumps -= 1;
+        }
+
+        if(rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up*Physics2D.gravity.y*(fallMultiplier - 1)*Time.deltaTime;
+        }
+
+        if(rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        if (rb.velocity.x < limit * -1)
+        {
+            rb.velocity = new Vector2(limit * -1, rb.velocity.y);
+        }
+        else if (rb.velocity.x > limit)
+        {
+            rb.velocity = new Vector2(limit, rb.velocity.y);
+        } else { 
+            rb.velocity += movement*speed;
+        }
+
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            jumps = 2;
+        }
+        if(collision.gameObject.CompareTag("BlackHole"))
+        {
+            //gameover;
+        }
+    }
+}
+
